31fdfce4815a8f3dc85e5ef4edbf25ea9b7aba1a
LinasBeres <beres.linas@gmail.com>
2024-04-18 12:10:16 -0700

    Moved db

--- pyMotoMaintence/importDB.py
+++ pyMotoMaintence/importDB.py
@@ -1,2 +1,10 @@
+import json
+
+from importlib.resources import files
+
+
 def importBD():
-    print("asdasd")
+    print(__file__)
+
+    with files('pyMotoMaintence.moto-maintenance-db.db.suzuki').joinpath('dl650_04-11.json').open('r') as fl:
+        json.load(fl)
