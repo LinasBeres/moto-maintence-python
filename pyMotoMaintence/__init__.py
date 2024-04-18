98f3c0962d1786858fcd2799be1c0762f56a688f
LinasBeres <beres.linas@gmail.com>
2024-04-18 14:18:11 -0700

    Added load funcs

--- pyMotoMaintence/__init__.py
+++ pyMotoMaintence/__init__.py
@@ -4,3 +4,4 @@ version = '0.0.1'
 __version__ = version
 
 import pyMotoMaintence.importDB as importDB
+import pyMotoMaintence.addMoto as addMoto
--- pyMotoMaintence/importDB.py
+++ pyMotoMaintence/importDB.py
@@ -1,10 +1,56 @@
 import json
 
+from pathlib import Path
 from importlib.resources import files
 
+_motoDatabase = 'pyMotoMaintence.moto-maintenance-db.db'
 
-def importBD():
-    print(__file__)
+def iterMotorcycleMakes() -> str:
+    for resource in files(_motoDatabase).iterdir():
+        if resource.is_dir():
+            yield resource.name
 
-    with files('pyMotoMaintence.moto-maintenance-db.db.suzuki').joinpath('dl650_04-11.json').open('r') as fl:
-        json.load(fl)
+def iterMotorcycleTypes(motorcycleMake: str) -> str:
+    for resource in files(_motoDatabase).joinpath(motorcycleMake).iterdir():
+        if resource.is_file():
+            yield Path(resource.name).stem
+
+def motorcycleMakeExists(motorcycleMake: str) -> bool:
+    for make in iterMotorcycleMakes():
+        if motorcycleMake.lower() == make:
+            return True
+
+    return False
+
+def motorcycleTypeExists(motorcycleMake: str, motorcycleType: str) -> bool:
+    for motoType in iterMotorcycleTypes(motorcycleMake):
+        if motorcycleType.lower() == motoType:
+            return True
+
+    return False
+
+def loadMotorcycle(motorcycleMake: str, motorcycleType: str, safe=True) -> dict:
+    outDict = {}
+
+    if safe:
+        if not motorcycleMakeExists(motorcycleMake):
+            print(f"Warning: motorcycle make '{motorcycleMake}' doesn't exist!")
+            return outDict
+        if not motorcycleTypeExists(motorcycleMake, motorcycleType):
+            print(f"Warning: motorcycle type '{motorcycleType}' doesn't exist!")
+            return outDict
+
+    makeDatabase = f'{_motoDatabase}.{motorcycleMake}.{motorcycleType}.json'
+
+    with files(makeDatabase).open('r') as fl:
+        outDict = json.load(fl)
+
+    return outDict
+
+def loadMotorcycles(motorcycles: dict, safe=True) -> dict:
+    motoDict = {}
+
+    for motoMake, motoType in motorcycles.items():
+        motoDict[motoType] = loadMotorcycle(motoMake, motoType, safe)
+
+    return motoDict
